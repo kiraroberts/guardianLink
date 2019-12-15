d277bdd3f2e37479b002af9549d88297924c53d0
Kira Roberts <rosehill1788@protonmail.com>
2019-12-15 10:18:03 -0800

    add id link to postcontent

--- src/components/posts/PostContent.js
+++ src/components/posts/PostContent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const PostContent = () => {
+const PostContent = (props) => {
+  const id = props.match.params.id;
   return(
     <div className="container section post-content">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Post Title</span>
+          <span className="card-title">Post Title {id}</span>
           <p>Content</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
