1e3e00c22a88dd10a5317ae85b15f4aa30ff439c
Kira Roberts <rosehill1788@protonmail.com>
2019-12-15 11:44:43 -0800

    add routes to navlinks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   return(
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to='/' className="brand-logo"></Link>
+        <Link to='/' className="brand-logo">Guardian Link</Link>
         <SignedInLinks />
         <SignedOutLinks />
       </div>
--- src/components/navbar/SignedInLinks.js
+++ src/components/navbar/SignedInLinks.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/">New Post</NavLink></li>
+      <li><NavLink to="/create">New Post</NavLink></li>
       <li><NavLink to="/">Log Out</NavLink></li>
       <li><NavLink to="/" className="btn btn-floating grey darken-1">Pic</NavLink></li>
     </ul>
