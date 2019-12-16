4eb53674f1dd8972ef9b67c59c85d10f5f56cdf2
Kira Roberts <rosehill1788@protonmail.com>
2019-12-15 16:37:03 -0800

    add firebase config to redux firestore and react redux firebase

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import firebaseConfig from './config/firebaseConfig';
 
 const store = createStore(rootReducer, 
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-    reduxFirestore(),
-    reactReduxFirebase()
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
   )
 );
 
